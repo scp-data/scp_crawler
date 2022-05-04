6fe3e9fd75fc0e7b735593f55d2f943323f9897d
Robert Hafner <tedivm@tedivm.com>
2022-05-03 22:07:31 -0500

    iso dates, images in metadata, more fields

--- scp_crawler/postprocessing.py
+++ scp_crawler/postprocessing.py
@@ -1,7 +1,18 @@
 import json
 import os
+from datetime import date, datetime
+
+from bs4 import BeautifulSoup
 
 cwd = os.getcwd()
+
+
+def json_serial(obj):
+    # Convert datetimes to strings in ISO format.
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    # Convert anything else to a string.
+    return str(obj)
 
 
 def from_file(path):
@@ -12,7 +23,7 @@ def from_file(path):
 
 def to_file(obj, path):
     with open(path, "w") as fs:
-        json.dump(obj, fs, sort_keys=True)
+        json.dump(obj, fs, sort_keys=True, default=json_serial)
 
 
 title_list = from_file(cwd + "/data/scp_titles.json")
@@ -26,6 +37,21 @@ for item in item_list:
     else:
         item["title"] = item["scp"]
     del item["link"]
+
+    content_soup = BeautifulSoup(item["raw_content"], "lxml")
+    img_tags = content_soup.find_all("img")
+    item["images"] = [img["src"] for img in img_tags]
+
+    # Convert history dict to list and sort by date.
+    item["history"] = [v for v in item["history"].values()]
+    for revision in item["history"]:
+        revision["date"] = datetime.strptime(revision["date"], "%d %b %Y %H:%M")
+    item["history"].sort(key=lambda x: x["date"])
+
+    if len(item["history"]) > 0:
+        item["created_at"] = item["history"][0]["date"]
+        item["creator"] = item["history"][0]["author"]
+
     items[item["scp"]] = item
 
 print("Saving indexed content.")
